5b3feb13a31b4ab232bfa4c04af0e851a807459d
Godwin Agbleze <98172807+godwin-17@users.noreply.github.com>
2023-07-31 12:11:59 +0200

    Create AddTodo component

---
+++ src/components/AddTodo/AddTodo.tsx
@@ -0,0 +1,14 @@
+import "./AddTodo.css";
+
+const AddTodo = () => {
+  return (
+    <>
+      <button className="add-btn">
+        <img src="src\assets\Adds.svg" alt="Add Button" className="add-icon" />
+        Nuova Voce
+      </button>
+    </>
+  );
+};
+
+export default AddTodo;
