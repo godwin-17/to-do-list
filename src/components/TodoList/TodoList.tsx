c4e00c8678070c8f6ce8a116aaa764338432d0ea
Godwin Agbleze <98172807+godwin-17@users.noreply.github.com>
2023-07-31 19:09:37 +0200

    Add todo list logic

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,34 @@
 import "./TodoList.css";
 import TodoItem from "../TodoItem/TodoItem";
 
-const TodoList = () => {
+interface TodoItem {
+  id: string;
+  text: string;
+  completed?: boolean;
+}
+
+interface Props {
+  todos: TodoItem[];
+  onCheckboxChange: (id: string, completed: boolean) => void;
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList = ({ todos, onCheckboxChange, onDeleteTodo }: Props) => {
   return (
     <div className="todo-list-container">
       <div className="todo-list-content">
         <h1 className="title">TODO</h1>
         <div className="todos-container">
-          <TodoItem />
+          {todos.map((todo) => (
+            <TodoItem
+              {...todo}
+              key={todo.id}
+              id={todo.id}
+              completed={todo.completed}
+              onCheckboxChange={onCheckboxChange}
+              onDelete={onDeleteTodo}
+            />
+          ))}
         </div>
       </div>
     </div>
