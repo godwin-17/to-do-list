add7f414d41a6253762e72c432a755737e596445
Godwin Agbleze <98172807+godwin-17@users.noreply.github.com>
2023-07-31 19:12:08 +0200

    Add logic

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,9 +1,13 @@
 import "./AddTodo.css";
 
-const AddTodo = () => {
+interface Props {
+  onModalVisibility: () => void;
+}
+
+const AddTodo = ({ onModalVisibility }: Props) => {
   return (
     <>
-      <button className="add-btn">
+      <button className="add-btn" onClick={onModalVisibility}>
         <img src="src\assets\Adds.svg" alt="Add Button" className="add-icon" />
         Nuova Voce
       </button>
