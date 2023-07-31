765036178b09a1b5f01d89a7ed64cf6f223db0d2
Godwin Agbleze <98172807+godwin-17@users.noreply.github.com>
2023-07-31 19:06:40 +0200

    Add app logic

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -2,13 +2,89 @@ import AddTodo from "../AddTodo/AddTodo";
 import TodoList from "../TodoList/TodoList";
 import Modal from "../Modal/Modal";
 import "./TodoApp.css";
+import { useEffect, useRef, useState } from "react";
+
+interface TodoItem {
+  id: string;
+  text: string;
+  completed?: boolean;
+}
 
 const TodoApp = () => {
+  const [modalVisibility, setModalVisibility] = useState<boolean>(false);
+  const [savedText, setSavedText] = useState<string>("");
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const counterRef = useRef<number>(0);
+
+  const handleModalVisibility = (visibility: boolean) => {
+    setModalVisibility(visibility);
+  };
+
+  const handleSavedText = (text: string) => {
+    setSavedText(text);
+  };
+
+  const handleSaveTodo = () => {
+    console.log(todos);
+
+    if (savedText.trim() !== "") {
+      const newTodo: TodoItem = {
+        id: `${counterRef.current}`,
+        text: savedText,
+        completed: false,
+      };
+
+      counterRef.current++;
+
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      setSavedText("");
+    }
+  };
+
+  const handleCheckboxChange = (id: string, completed: boolean) => {
+    setTodos((prevTodos) => {
+      const updatedTodos: TodoItem[] = prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed } : todo
+      );
+
+      updatedTodos.sort((a, b) => {
+        if (a.completed && !b.completed) return 1;
+        if (!a.completed && b.completed) return -1;
+        return 0;
+      });
+
+      return updatedTodos;
+    });
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    if (modalVisibility) {
+      document.body.classList.add("active-modal");
+    } else {
+      document.body.classList.remove("active-modal");
+    }
+  }, [modalVisibility]);
+
   return (
     <div className="todo-app">
-      <Modal />
-      <TodoList />
-      <AddTodo />
+      {modalVisibility && (
+        <Modal
+          onModalVisibility={handleModalVisibility}
+          onSaveText={handleSavedText}
+          onSaveTodo={handleSaveTodo}
+        />
+      )}
+
+      <TodoList
+        todos={todos}
+        onCheckboxChange={handleCheckboxChange}
+        onDeleteTodo={handleDeleteTodo}
+      />
+      <AddTodo onModalVisibility={() => setModalVisibility(true)} />
     </div>
   );
 };
