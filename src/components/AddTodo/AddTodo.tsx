1c0da2c1b6201da72bd2b3b998cadb7ddfad632e
Godwin Agbleze <98172807+godwin-17@users.noreply.github.com>
2023-08-01 09:24:46 +0200

    Fix SVG button

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -8,7 +8,26 @@ const AddTodo = ({ onModalVisibility }: Props) => {
   return (
     <>
       <button className="add-btn" onClick={onModalVisibility}>
-        <img src="src\assets\Adds.svg" alt="Add Button" className="add-icon" />
+        <svg
+          className="add-icon"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M33 20L7 20"
+            stroke="#01001e"
+            strokeWidth="3"
+            strokeLinecap="round"
+          />
+          <path
+            d="M20 33L20 7"
+            stroke="#01001e"
+            strokeWidth="3"
+            strokeLinecap="round"
+          />
+        </svg>
         Nuova Voce
       </button>
     </>
